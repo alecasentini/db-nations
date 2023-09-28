d087e2e0bacd1b377f3ee3174ec9c4ba81a24a95
alecasentini <ale.casentini@gmail.com>
2023-09-28 15:52:35 +0200

    Milestone 1 - 2

--- src/main/java/org/java/nation/Main.java
+++ src/main/java/org/java/nation/Main.java
@@ -2,6 +2,8 @@ package org.java.nation;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
 
@@ -14,6 +16,29 @@ public class Main {
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			
 			System.out.println("Connessione stabilita correttamente");
+			
+			Statement statement = conn.createStatement();
+			
+			String query = "SELECT c.NAME AS Nazione, c.COUNTRY_ID AS ID, r.NAME AS Regione, ct.NAME AS Continente " +
+                    "FROM COUNTRIES c " +
+                    "JOIN REGIONS R ON c.REGION_ID = r.REGION_ID " +
+                    "JOIN CONTINENTS ct ON r.CONTINENT_ID = ct.CONTINENT_ID " +
+                    "ORDER BY c.NAME";
+			
+			ResultSet resultSet = statement.executeQuery(query);
+			
+			while (resultSet.next()) {
+                String nazione = resultSet.getString("Nazione");
+                int id = resultSet.getInt("ID");
+                String regione = resultSet.getString("Regione");
+                String continente = resultSet.getString("Continente");
+
+                System.out.println("Nazione: " + nazione);
+                System.out.println("ID: " + id);
+                System.out.println("Regione: " + regione);
+                System.out.println("Continente: " + continente);
+                System.out.println("----------------------------------");
+            }
 		} catch (Exception e) {
 			
 			System.out.println("Errore di connessione: " + e.getMessage());
